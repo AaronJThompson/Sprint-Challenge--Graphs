b87aea7b1b823c7bf65861f27d44d8a95c1798e5
AaronJThompson <aaronjthompson98@gmail.com>
2019-11-08 09:46:25 +0000

    Fix iteration

--- graph_adventure/adv.py
+++ graph_adventure/adv.py
@@ -9,7 +9,7 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 
-# roomGraph={0: [(3, 5), {'n': 1}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}]}
+#roomGraph={0: [(3, 5), {'n': 1}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}]}
 # roomGraph={0: [(3, 5), {'n': 1, 's': 5, 'e': 3, 'w': 7}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}], 3: [(4, 5), {'w': 0, 'e': 4}], 4: [(5, 5), {'w': 3}], 5: [(3, 4), {'n': 0, 's': 6}], 6: [(3, 3), {'n': 5}], 7: [(2, 5), {'w': 8, 'e': 0}], 8: [(1, 5), {'e': 7}]}
 # roomGraph={0: [(3, 5), {'n': 1, 's': 5, 'e': 3, 'w': 7}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}], 3: [(4, 5), {'w': 0, 'e': 4}], 4: [(5, 5), {'w': 3}], 5: [(3, 4), {'n': 0, 's': 6}], 6: [(3, 3), {'n': 5, 'w': 11}], 7: [(2, 5), {'w': 8, 'e': 0}], 8: [(1, 5), {'e': 7}], 9: [(1, 4), {'n': 8, 's': 10}], 10: [(1, 3), {'n': 9, 'e': 11}], 11: [(2, 3), {'w': 10, 'e': 6}]}
 # roomGraph={0: [(3, 5), {'n': 1, 's': 5, 'e': 3, 'w': 7}], 1: [(3, 6), {'s': 0, 'n': 2, 'e': 12, 'w': 15}], 2: [(3, 7), {'s': 1}], 3: [(4, 5), {'w': 0, 'e': 4}], 4: [(5, 5), {'w': 3}], 5: [(3, 4), {'n': 0, 's': 6}], 6: [(3, 3), {'n': 5, 'w': 11}], 7: [(2, 5), {'w': 8, 'e': 0}], 8: [(1, 5), {'e': 7}], 9: [(1, 4), {'n': 8, 's': 10}], 10: [(1, 3), {'n': 9, 'e': 11}], 11: [(2, 3), {'w': 10, 'e': 6}], 12: [(4, 6), {'w': 1, 'e': 13}], 13: [(5, 6), {'w': 12, 'n': 14}], 14: [(5, 7), {'s': 13}], 15: [(2, 6), {'e': 1, 'w': 16}], 16: [(1, 6), {'n': 17, 'e': 15}], 17: [(1, 7), {'s': 16}]}
@@ -55,18 +55,18 @@ def createTraversalPath():
     lastDir = None
     lastRoom = None
     while len(adjacency) < len(world.rooms):
-        if player.currentRoom not in adjacency:
+        if player.currentRoom.id not in adjacency:
             adj = dict()
             for ext in player.currentRoom.getExits():
                 adj[ext] = "?"
-            adjacency[player.currentRoom] = adj
+            adjacency[player.currentRoom.id] = adj
         if lastRoom:
-            adjacency[lastRoom][lastDir] = player.currentRoom
-            adjacency[player.currentRoom][reverseDir(lastDir)] = lastRoom
-        lastRoom = player.currentRoom
+            adjacency[lastRoom][lastDir] = player.currentRoom.id
+            adjacency[player.currentRoom.id][reverseDir(lastDir)] = lastRoom
+        lastRoom = player.currentRoom.id
         
         moved = False
-        for ext, room in enumerate(adjacency[player.currentRoom]):
+        for ext, room in adjacency[player.currentRoom.id].items():
             if room == "?":
                 lastDir = ext
                 traversal.push(ext)
@@ -80,6 +80,7 @@ def createTraversalPath():
             lastDir = ext
             player.travel(ext)
 
+createTraversalPath()
 
 # TRAVERSAL TEST
 visited_rooms = set()
